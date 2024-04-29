64be0dba618d15af0539899ec5ce23d111b84bae
DadouneIkram <i.dadoune@esi-sba.dz>
2024-04-29 16:57:48 +0100

    vers1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,31 +4,31 @@ import Cards from '@/components/dashboard/cards/cards'
 import Charte from '@/components/dashboard/charte/charte'
 import Pourcentage from '@/components/dashboard/pourcentage/pourcentage'
 interface prop {
-card1:"E3F6E0";
-card2:'E6F4F6';
-        card3:'EEF6F7';
+  card1: "E3F6E0";
+  card2: 'E6F4F6';
+  card3: 'EEF6F7';
 }
 
 
-const page: React.FC<prop> = ({card1,card2,card3}) => {
+const page: React.FC<prop> = ({ card1, card2, card3 }) => {
   return (
-  <div className='flex gap-10 mt-8'>
+    <div className='flex gap-10 mt-8'>
       <div className='flex-3 flex flex-col gap-8'>
         <div className='flex gap-8 justify-between'>
-          <Cards title="Users" color={card1} total='7,234' pourcentage='8,2'/>
-          <Cards title="Candidats" color={card2} total='4,100' pourcentage='20,2'/>
-          <Cards title="Pilgrims" color={card3} total='5,920' pourcentage='4,2'/>
+          <Cards title="Users" color={card1} total='7,234' pourcentage='8,2' />
+          <Cards title="Candidats" color={card2} total='4,100' pourcentage='20,2' />
+          <Cards title="Pilgrims" color={card3} total='5,920' pourcentage='4,2' />
         </div>
-      <Charte/>
-      <div className='flex gap-4 justify-around'>
-          <Pourcentage/>
-          <Barcharte/>
-      </div>
+        <Charte />
+        <div className='flex gap-4 justify-around'>
+          <Pourcentage />
+          <Barcharte />
+        </div>
       </div>
       <div className='flex-1'>
-        <Rank/>
+        <Rank />
       </div>
-  </div>
+    </div>
   )
 }
 
--- src/components/dashboard/navbar/navbar.tsx
+++ src/components/dashboard/navbar/navbar.tsx
@@ -45,10 +45,10 @@ function navbar() {
       </form>
       <div className="flex gap-2">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g clip-path="url(#clip0_844_18452)">
-            <path d="M8 9H16" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M8 13H14" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M18 4C18.7956 4 19.5587 4.31607 20.1213 4.87868C20.6839 5.44129 21 6.20435 21 7V15C21 15.7956 20.6839 16.5587 20.1213 17.1213C19.5587 17.6839 18.7956 18 18 18H13L8 21V18H6C5.20435 18 4.44129 17.6839 3.87868 17.1213C3.31607 16.5587 3 15.7956 3 15V7C3 6.20435 3.31607 5.44129 3.87868 4.87868C4.44129 4.31607 5.20435 4 6 4H18Z" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <g clipPath="url(#clip0_844_18452)">
+            <path d="M8 9H16" stroke="#2A2A2A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M8 13H14" stroke="#2A2A2A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M18 4C18.7956 4 19.5587 4.31607 20.1213 4.87868C20.6839 5.44129 21 6.20435 21 7V15C21 15.7956 20.6839 16.5587 20.1213 17.1213C19.5587 17.6839 18.7956 18 18 18H13L8 21V18H6C5.20435 18 4.44129 17.6839 3.87868 17.1213C3.31607 16.5587 3 15.7956 3 15V7C3 6.20435 3.31607 5.44129 3.87868 4.87868C4.44129 4.31607 5.20435 4 6 4H18Z" stroke="#2A2A2A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </g>
           <defs>
             <clipPath id="clip0_844_18452">
@@ -58,9 +58,9 @@ function navbar() {
         </svg>
 
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g clip-path="url(#clip0_844_18457)">
-            <path d="M10 5C10 4.46957 10.2107 3.96086 10.5858 3.58579C10.9609 3.21071 11.4696 3 12 3C12.5304 3 13.0391 3.21071 13.4142 3.58579C13.7893 3.96086 14 4.46957 14 5C15.1484 5.54303 16.1274 6.38833 16.832 7.4453C17.5367 8.50227 17.9404 9.73107 18 11V14C18.0753 14.6217 18.2954 15.2171 18.6428 15.7381C18.9902 16.2592 19.4551 16.6914 20 17H4C4.54494 16.6914 5.00981 16.2592 5.35719 15.7381C5.70457 15.2171 5.92474 14.6217 6 14V11C6.05956 9.73107 6.4633 8.50227 7.16795 7.4453C7.8726 6.38833 8.85159 5.54303 10 5Z" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M9 17V18C9 18.7956 9.31607 19.5587 9.87868 20.1213C10.4413 20.6839 11.2044 21 12 21C12.7956 21 13.5587 20.6839 14.1213 20.1213C14.6839 19.5587 15 18.7956 15 18V17" stroke="#2A2A2A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <g clipPath="url(#clip0_844_18457)">
+            <path d="M10 5C10 4.46957 10.2107 3.96086 10.5858 3.58579C10.9609 3.21071 11.4696 3 12 3C12.5304 3 13.0391 3.21071 13.4142 3.58579C13.7893 3.96086 14 4.46957 14 5C15.1484 5.54303 16.1274 6.38833 16.832 7.4453C17.5367 8.50227 17.9404 9.73107 18 11V14C18.0753 14.6217 18.2954 15.2171 18.6428 15.7381C18.9902 16.2592 19.4551 16.6914 20 17H4C4.54494 16.6914 5.00981 16.2592 5.35719 15.7381C5.70457 15.2171 5.92474 14.6217 6 14V11C6.05956 9.73107 6.4633 8.50227 7.16795 7.4453C7.8726 6.38833 8.85159 5.54303 10 5Z" stroke="#2A2A2A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M9 17V18C9 18.7956 9.31607 19.5587 9.87868 20.1213C10.4413 20.6839 11.2044 21 12 21C12.7956 21 13.5587 20.6839 14.1213 20.1213C14.6839 19.5587 15 18.7956 15 18V17" stroke="#2A2A2A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </g>
           <defs>
             <clipPath id="clip0_844_18457">
